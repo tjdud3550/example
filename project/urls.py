0de4983830a253404f6b5e458f1f275644b932e9
tjdud3550 <tjdud3550@likelion.org>
2019-06-24 00:55:56 +0900

    first commit

--- project/urls.py
+++ project/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
